a573d089f3d07f20fbc6f25d235db97567911e09
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-10-14 21:57:25 -0300

    update-KafkaService

--- Alura_ecommerce/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ Alura_ecommerce/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class KafkaService {
     private final KafkaConsumer<String, String> consumer;
@@ -36,6 +37,7 @@ public class KafkaService {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         return properties;
     }
 }
