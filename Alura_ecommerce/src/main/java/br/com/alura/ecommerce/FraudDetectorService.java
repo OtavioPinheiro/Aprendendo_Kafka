6b30d4e0e030caccb5932b5d89e334a42080fbc3
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-10-14 21:57:06 -0300

    update-FraudDetectorService

--- Alura_ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ Alura_ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,6 +1,7 @@
 package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -11,41 +12,33 @@ import java.util.UUID;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        //Obs.: É super raro ter um consumer escutando de vários tópicos,
-        // pois cada serviço irá ter um objetivo específico. Mas é possível.
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("Found " + records.count() + " registers");
-                for (var record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5_000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
-                }
-            }
-        }
+        var fraudService = new FraudDetectorService();
+        var service = new KafkaService("ECOMMERCE_NEW_ORDER",
+                fraudService::parse);
+        service.run();
     }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
+    }
+
+    //Obs.: É super raro ter um consumer escutando de vários tópicos,
+        // pois cada serviço irá ter um objetivo específico. Mas é possível.
 
     private static Properties properties() {
         var properties = new Properties();
-
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
         return properties;
     }
 }
