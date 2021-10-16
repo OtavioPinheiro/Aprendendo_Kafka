5ae511c5214fd8102ef245a236adfbcf56cade63
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-10-15 22:11:28 -0300

    update-FraudDetectorService

--- Alura_ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ Alura_ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -5,10 +5,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 public class FraudDetectorService {
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+        try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
-                fraudService::parse);
-        service.run();
+                fraudService::parse)) {
+            service.run();
+        }
     }
 
     private void parse(ConsumerRecord<String, String> record) {
