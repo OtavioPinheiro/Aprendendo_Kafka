864d032341ab36d976cd244b627ff09ee5869500
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-10-17 18:32:06 -0300

    feat-criação de um Map para passar propriedades extras ao Kafka

--- Alura_ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ Alura_ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -2,13 +2,16 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+
 public class EmailService {
     public static void main(String[] args) {
         var emailService = new EmailService();
         try(var service = new KafkaService(EmailService.class.getSimpleName(),
                 "ECOMMERCE_SEND_EMAIL",
                 emailService::parse,
-                String.class)) {
+                String.class,
+                new HashMap<>())) {
             service.run();
         }
     }
--- Alura_ecommerce/src/main/java/br/com/alura/ecommerce/LogService.java
+++ Alura_ecommerce/src/main/java/br/com/alura/ecommerce/LogService.java
@@ -2,11 +2,9 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.time.Duration;
-import java.util.Properties;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class LogService {
@@ -15,7 +13,8 @@ public class LogService {
         try (var service = new KafkaService(LogService.class.getSimpleName(),
                 Pattern.compile("ECOMMERCE.*"),
                 logService::parse,
-                String.class)) {
+                String.class,
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
             service.run();
         }
     }
