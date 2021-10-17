7adc83d49ff5e20b287139ed3d5d6ccdb943119c
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-10-17 18:06:46 -0300

    update-NewOrderMain

--- Alura_ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ Alura_ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -8,7 +8,7 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
                 for (var i = 0; i < 20; i++) {
                     var userId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
@@ -17,7 +17,7 @@ public class NewOrderMain {
                     var order = new Order(userId, orderId, value);
                     orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                    var email = "Thank you for your order! We are processing your order!";
+                    var email = new Email("Acknowledgement", "Thank you for your order! We are processing your order!");
                     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
                 }
             }
